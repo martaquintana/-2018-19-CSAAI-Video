7c1c035132fc927f6e9359d6d5e4f03e8143c637
Marta <Marta@MacBook-Pro-de-Marta.local>
2019-04-10 21:19:12 +0200

    Añadiendo borde a botones al pulsar y pasar por encima videos

--- video.js
+++ video.js
@@ -3,7 +3,7 @@ function main()
   var display;
 
 
-  console.log("Video 01...")
+  console.log("Realizador...")
   video1 = document.getElementById("video1")
   video2 = document.getElementById("video2")
   video3 = document.getElementById("video3")
@@ -32,47 +32,72 @@ var v = [
         "https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente3.mp4"
         ];
 
-
   video1.onmouseover = () => {
           console.log("Mouse over 1!!");
-          video1.muted=false;
+          //video1.muted=false;
+          video1.style.borderStyle = "double";
   }
 
   video1.onmouseout = () => {
           console.log("Mouse out 1!!");
-          video1.muted= true;
+          //video1.muted= true;
+          video1.style.borderStyle = "";
   }
 
   video2.onmouseover = () => {
           console.log("Mouse over 2!!");
-          video2.muted=false;
+          //video2.muted=false;
+          video2.style.borderStyle = "double";
   }
   video2.onmouseout = () => {
           console.log("Mouse out 2!!");
-          video2.muted=true;
+          //video2.muted=true;
+          video2.style.borderStyle = "";
   }
 
   video3.onmouseover = () => {
           console.log("Mouse over 3!!");
-          video3.muted=false;
+          //video3.muted=false;
+          video3.style.borderStyle = "double";
   }
-  video3.onmouseover = () => {
+  video3.onmouseout = () => {
           console.log("Mouse out 3!!");
-          video3.muted=true;
+          //video3.muted=true;
+          video3.style.borderStyle = "";
   }
 
 
   boton1.onclick=()=>{
     cambiarvideo(0);
-    display.currentTime= video1.currentTime;
+    display.currentTime = video1.currentTime;
+    video1.style.borderColor= "red";
+    video2.style.borderColor= "";
+    video3.style.borderColor= "";
+    boton1.style.borderStyle = "double";
+    boton2.style.borderStyle = "";
+    boton3.style.borderStyle = "";
   }
   boton2.onclick=()=>{
     cambiarvideo(1);
-    display.currentTime= video2.currentTime;
+    display.currentTime = video2.currentTime;
+    video1.style.borderColor= "";
+    video2.style.borderColor= "red";
+    video3.style.borderColor= "";
+    boton1.style.borderStyle = "";
+    boton2.style.borderStyle = "double";
+    boton3.style.borderStyle = "";
+
   }
   boton3.onclick=()=>{
     cambiarvideo(2);
-    display.currentTime= video3.currentTime;}
+    display.currentTime = video3.currentTime;
+    video1.style.borderColor= "";
+    video2.style.borderColor= "";
+    video3.style.borderColor= "red";
+    boton1.style.borderStyle = "";
+    boton2.style.borderStyle = "";
+    boton3.style.borderStyle = "double";
+  }
 
 function cambiarvideo(n){
     console.log(v[n])
@@ -81,5 +106,6 @@ function cambiarvideo(n){
     display.load();
     display.setAttribute('src', v[n]);
     display.play();
+
   }
 }
